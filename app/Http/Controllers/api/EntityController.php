15b25ff2046d3495fa1f49af576172dfd414afa3
Ramsés Moreno <ramses@cuatromedios.com>
2018-01-16 18:45:56 -0600

    Homogenized the results of the API to always return and object with success, data and info properties

--- app/Http/Controllers/api/EntityController.php
+++ app/Http/Controllers/api/EntityController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\api;
 
+use App\Exceptions\ExceptionDetails;
 use App\Http\Controllers\Controller;
 use App\Models\Entity;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Config;
+use App\Models\ApiResponse;
 
 class EntityController extends Controller
 {
@@ -24,161 +26,195 @@ class EntityController extends Controller
      * Return the specified entity.
      *
      * @param $id
-     * @return \Illuminate\Http\JsonResponse|string
+     * @return \Illuminate\Http\Response
      */
     public function getOne($id, Request $request)
     {
-        $lang = $request->input('lang', Config::get('general.langs')[0]);
-        $fields = $request->input('fields', []);
-        $entity = Entity::getOne($id, $fields, $lang);
-
-        if (!$entity instanceof Entity) {
-            return $this->sendNotFoundResponse("The entity with id {$id} doesn't exist");
+        try {
+            $lang = $request->input('lang', Config::get('general.langs')[0]);
+            $fields = $request->input('fields', []);
+            $entity = Entity::getOne($id, $fields, $lang);
+            return (new ApiResponse($entity, TRUE))->response();
+        } catch (\Exception $e) {
+            $status = 404;
+            return (new ApiResponse(NULL, FALSE, ExceptionDetails::filter($e), $status))->response();
         }
-        return $entity;
     }
 
     /**
      * Create the specified entity.
      *
      * @param $id
-     * @return \Illuminate\Http\JsonResponse|string
+     * @return \Illuminate\Http\Response
      */
     public function post(Request $request)
     {
-        // TODO: Filter the json to delete al not used data
-        $id = Entity::post($request->json()->all());
-        return ["data" => $id];
+        try {
+            // TODO: Filter the json to delete al not used data
+            $id = Entity::post($request->json()->all());
+            return (new ApiResponse($id, TRUE))->response();
+        } catch (\Exception $e) {
+            $status = 400;
+            return (new ApiResponse(NULL, FALSE, ExceptionDetails::filter($e), $status))->response();
+        }
     }
 
     /**
      * Update the specified entity.
      *
      * @param $id
-     * @return \Illuminate\Http\JsonResponse|string
+     * @return \Illuminate\Http\Response
      */
     public function patch($id, Request $request)
     {
-        // TODO: Filter the json to delete al not used data
-        $id = Entity::patch($id, $request->json()->all());
-        return ["data" => $id];
+        try {
+            // TODO: Filter the json to delete al not used data
+            $id = Entity::patch($id, $request->json()->all());
+            return (new ApiResponse($id, TRUE))->response();
+
+        } catch (\Exception $e) {
+            $status = 400;
+            return (new ApiResponse(NULL, FALSE, ExceptionDetails::filter($e), $status))->response();
+        }
     }
 
     /**
      * Display entity's parent.
      *
      * @param $id
-     * @return \Illuminate\Http\JsonResponse|string
+     * @return \Illuminate\Http\Response
      */
     public function getParent($id, Request $request)
     {
-        $lang = $request->input('lang', Config::get('general.langs')[0]);
-        $fields = $request->input('fields', []);
-        $entity = Entity::getParent($id, $fields, $lang);
-
-        if (!$entity instanceof Entity) {
-            return $this->sendNotFoundResponse("The entity with id {$id} doesn't exist");
+        try {
+            $lang = $request->input('lang', Config::get('general.langs')[0]);
+            $fields = $request->input('fields', []);
+            $entity = Entity::getParent($id, $fields, $lang);
+            if (!$entity instanceof Entity) {
+                return $this->sendNotFoundResponse("The entity with id {$id} doesn't exist");
+            }
+            return (new ApiResponse($entity, TRUE))->response();
+        } catch (\Exception $e) {
+            $status = 404;
+            return (new ApiResponse(NULL, FALSE, ExceptionDetails::filter($e), $status))->response();
         }
-        return $entity;
     }
 
     /**
      * Display all entities.
      *
      * @param $id
-     * @return \Illuminate\Http\JsonResponse|string
+     * @return \Illuminate\Http\Response
      */
     public function get(Request $request)
     {
-        $fields = $request->input('fields', []);
-        $lang = $request->input('lang', Config::get('general.langs')[0]);
-
-        $collection = Entity::get(NULL, $fields, $lang);
-
-        return $collection;
+        try {
+            $fields = $request->input('fields', []);
+            $lang = $request->input('lang', Config::get('general.langs')[0]);
+            $collection = Entity::get(NULL, $fields, $lang);
+            return (new ApiResponse($collection, TRUE))->response();
+        } catch (\Exception $e) {
+            $status = 500;
+            return (new ApiResponse(NULL, FALSE, ExceptionDetails::filter($e), $status))->response();
+        }
     }
 
     /**
      * Display entity's children.
      *
      * @param $id
-     * @return \Illuminate\Http\JsonResponse|string
+     * @return \Illuminate\Http\Response
      */
     public function getChildren($id, Request $request)
     {
-        $fields = $request->input('fields', []);
-        $lang = $request->input('lang', Config::get('general.langs')[0]);
-        $order = $request->input('order', NULL);
-
-        $collection = Entity::getChildren($id, $fields, $lang, $order);
-
-        return $collection;
+        try {
+            $fields = $request->input('fields', []);
+            $lang = $request->input('lang', Config::get('general.langs')[0]);
+            $order = $request->input('order', NULL);
+            $collection = Entity::getChildren($id, $fields, $lang, $order);
+            return (new ApiResponse($collection, TRUE))->response();
+        } catch (\Exception $e) {
+            $status = 500;
+            return (new ApiResponse(NULL, FALSE, ExceptionDetails::filter($e), $status))->response();
+        }
     }
 
     /**
      * Display entity's ancestors.
      *
      * @param $id
-     * @return \Illuminate\Http\JsonResponse|string
+     * @return \Illuminate\Http\Response
      */
     public function getAncestors($id, Request $request)
     {
-        $fields = $request->input('fields', []);
-        $lang = $request->input('lang', Config::get('general.langs')[0]);
-        $order = $request->input('order', NULL);
-
-        $collection = Entity::getAncestors($id, $fields, $lang, $order);
-
-        return $collection;
+        try {
+            $fields = $request->input('fields', []);
+            $lang = $request->input('lang', Config::get('general.langs')[0]);
+            $order = $request->input('order', NULL);
+            $collection = Entity::getAncestors($id, $fields, $lang, $order);
+            return (new ApiResponse($collection, TRUE))->response();
+        } catch (\Exception $e) {
+            $status = 500;
+            return (new ApiResponse(NULL, FALSE, ExceptionDetails::filter($e), $status))->response();
+        }
     }
 
     /**
      * Display entity's ancestors.
      *
      * @param $id
-     * @return \Illuminate\Http\JsonResponse|string
+     * @return \Illuminate\Http\Response
      */
     public function getDescendants($id, Request $request)
     {
-        $fields = $request->input('fields', []);
-        $lang = $request->input('lang', Config::get('general.langs')[0]);
-        $order = $request->input('order', NULL);
-
-        $collection = Entity::getDescendants($id, $fields, $lang, $order);
-
-        return $collection;
+        try {
+            $fields = $request->input('fields', []);
+            $lang = $request->input('lang', Config::get('general.langs')[0]);
+            $order = $request->input('order', NULL);
+            $collection = Entity::getDescendants($id, $fields, $lang, $order);
+            return (new ApiResponse($collection, TRUE))->response();
+        } catch (\Exception $e) {
+            $status = 500;
+            return (new ApiResponse(NULL, FALSE, ExceptionDetails::filter($e), $status))->response();
+        }
     }
 
     /**
      * Display entity's relations.
      *
      * @param $id
-     * @return \Illuminate\Http\JsonResponse
+     * @return \Illuminate\Http\Response
      */
     public function getRelations($id, $kind = NULL, Request $request)
     {
-        $fields = $request->input('fields', []);
-        $lang = $request->input('lang', Config::get('general.langs')[0]);
-        $order = $request->input('order', NULL);
-
-        $collection = Entity::getEntityRelations($id, $kind, $fields, $lang, $order);
-
-        return $collection;
+        try {
+            $fields = $request->input('fields', []);
+            $lang = $request->input('lang', Config::get('general.langs')[0]);
+            $order = $request->input('order', NULL);
+            $collection = Entity::getEntityRelations($id, $kind, $fields, $lang, $order);
+            return (new ApiResponse($collection, TRUE))->response();
+        } catch (\Exception $e) {
+            $status = 500;
+            return (new ApiResponse(NULL, FALSE, ExceptionDetails::filter($e), $status))->response();
+        }
     }
     /**
      * Display entity's relations.
      *
      * @param $id
-     * @return \Illuminate\Http\JsonResponse
+     * @return \Illuminate\Http\Response
      */
     public function getInverseRelations($id, $kind = NULL, Request $request)
     {
-        $fields = $request->input('fields', []);
-        $lang = $request->input('lang', Config::get('general.langs')[0]);
-        $order = $request->input('order', NULL);
-
-        $collection = Entity::getInverseEntityRelations($id, $kind, $fields, $lang, $order);
-
-        return $collection;
+        try {
+            $fields = $request->input('fields', []);
+            $lang = $request->input('lang', Config::get('general.langs')[0]);
+            $order = $request->input('order', NULL);
+            $collection = Entity::getInverseEntityRelations($id, $kind, $fields, $lang, $order);
+            return (new ApiResponse($collection, TRUE))->response();
+        } catch (\Exception $e) {
+            $status = 500;
+            return (new ApiResponse(NULL, FALSE, ExceptionDetails::filter($e), $status))->response();
+        }
     }
 }
