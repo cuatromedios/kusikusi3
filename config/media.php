5c4d0b6f56506bf92e7f42070dccff4c13619aa9
Ramsés Moreno <ramses@cuatromedios.com>
2018-02-26 17:48:57 -0600

    Presets are enclosed in a preset property to allow have more configuration keys in the same media file

--- config/media.php
+++ config/media.php
@@ -1,15 +1,17 @@
 <?php
 
 return [
-    'thumb' => [
-        'quality' => 85,
-        'width' => 256,
-        'height' =>  256,
-        'background' => 'crop',
-        'alignment' => 'center',
-        'scale' => 'contain',
-        'format' => 'jpg',
-        'effects' => []
+    'presets' => [
+        'thumb' => [
+            'quality' => 85,
+            'width' => 256,
+            'height' =>  256,
+            'background' => 'crop',
+            'alignment' => 'center',
+            'scale' => 'contain',
+            'format' => 'jpg',
+            'effects' => []
+        ]
     ]
 ];
 
