f88cb2b0888a39e0a3a7bebe9fa98c47e1147e32
holtzheimer <joshua@cuatromedios.com>
2018-07-31 12:17:06 -0500

    Added SCSSphp as a compiler for .scss files.

--- app/Controllers/Web/HtmlController.php
+++ app/Controllers/Web/HtmlController.php
@@ -5,11 +5,19 @@ namespace App\Controllers\Web;
 use Cuatromedios\Kusikusi\Http\Controllers\Controller;
 use Cuatromedios\Kusikusi\Models\Entity;
 use Illuminate\Http\Request;
+use Leafo\ScssPhp\Compiler;
 
 class HtmlController extends Controller
 {
 
     public function home (Request $request, Entity $entity) {
+
+        $scss = new Compiler();
+        $scss->setFormatter('Leafo\ScssPhp\Formatter\Compressed');
+        $scss->setImportPaths(resource_path('views/styles/'));
+        $css = $scss->compile(file_get_contents(resource_path('views/styles/main.scss')));
+        file_put_contents (base_path('public/styles/main.css') , $css );
+
         $children = Entity::getChildren($entity['id']);
         return view('html.home', [
             'entity' => $entity,
--- resources/views/html/home.blade.php
+++ resources/views/html/home.blade.php
@@ -1,6 +1,8 @@
 @extends('html')
 @section('title', $entity['contents']['title'])
 @section('main')
+
+    <link rel="stylesheet" href="/styles/main.css">
     <h1>{{ $entity['contents']['title'] }}</h1>
     <p>{{ $entity['contents']['description'] }}</p>
     <ul>
