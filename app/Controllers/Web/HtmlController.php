d950dd55f85f6d9e9f0a1ddbea81822d506a0828
holtzheimer <joshua@cuatromedios.com>
2018-08-03 12:08:34 -0500

    Multiple .scss files compilations

--- app/Controllers/Web/HtmlController.php
+++ app/Controllers/Web/HtmlController.php
@@ -12,11 +12,16 @@ class HtmlController extends Controller
 
     public function home (Request $request, Entity $entity) {
 
-        $scss = new Compiler();
-        $scss->setFormatter('Leafo\ScssPhp\Formatter\Compressed');
-        $scss->setImportPaths(resource_path('views/styles/'));
-        $css = $scss->compile(file_get_contents(resource_path('views/styles/main.scss')));
-        file_put_contents (base_path('public/styles/main.css') , $css );
+        $scssFiles = scandir(resource_path('views/styles/'));
+        for ($f = 2; $f < count($scssFiles); $f++) {
+            $scss = new Compiler();
+            $scss->setFormatter('Leafo\ScssPhp\Formatter\Compressed');
+            // $scss->addImportPath(resource_path('views/styles/css'));
+            $scss->addImportPath(base_path('vendor/zurb/foundation/scss'));
+            $css = $scss->compile(file_get_contents(resource_path('views/styles/'.$scssFiles[$f])));
+            $cssFiles = str_replace('scss','css',$scssFiles[$f]);
+            file_put_contents (base_path('public/styles/'.$cssFiles), $css );
+        }
 
         $children = Entity::getChildren($entity['id']);
         return view('html.home', [
@@ -29,7 +34,7 @@ class HtmlController extends Controller
         return view('html.section', [
             'entity' => $entity,
             'children' => $children
-            ]);
+        ]);
     }
     public function page (Request $request, Entity $entity) {
         return view('html.page', ['entity' => $entity]);
