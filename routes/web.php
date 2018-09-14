45868817778b641f3f9c3aa2e6c3a2b6cfeb7c64
Ramses <ramses@cuatromedios.com>
2018-09-14 10:25:32 -0500

    Sample of redirects if doing an upgrade of the website.

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,22 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web unique route
+| Web routes
 |--------------------------------------------------------------------------
 |
+| This is the last routes called, after /media and /api.
 | There is only one route capturing all web requests. You are free to add
-| other routes as needed.
+| other routes as needed, for example to redirect previeous version of a site.
 |
 */
+
+// Place here site's previous versions urls and where should they go now. (301 = permanent, 302 = temporal)
+
+// Route::get('previous/version', function () {  return redirect('new/version', 301);  });
+
+
+
+// Catch any other routes
 
 $router->group(['namespace' => 'Cuatromedios\Kusikusi\Http\Controllers\Web'], function () use ($router) {
 
