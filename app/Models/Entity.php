0a5a071a389f035154020e49adefd01cb5efcff0
Ramses <ramses@cuatromedios.com>
2018-12-11 14:29:02 -0600

    Reduced the models complexity to use more Eloquent's features

--- app/Models/Entity.php
+++ app/Models/Entity.php
@@ -7,21 +7,10 @@ use Cuatromedios\Kusikusi\Models\EntityModel;
 class Entity extends EntityModel
 {
 
-  public function __construct(array $attributes = [])
-  {
-    parent::__construct($attributes);
-  }
-
   /**
    * Create an 1:1 relation for every EntityModel in the app
    */
   public function user() {
     return $this->hasOne('App\Models\User', 'id');
-  }
-  public function root() {
-    return $this->hasOne('App\Models\Root', 'id');
-  }
-  public function home() {
-    return $this->hasOne('App\Models\Home', 'id');
   }
 }
--- database/seeds/RootSeeder.php
+++ database/seeds/RootSeeder.php
@@ -13,68 +13,11 @@ class RootSeeder extends Seeder
 
   public function run()
   {
-
-    $root = new \App\Models\Root([
+    $root = new Entity ([
         "id" => "root",
-        "entity" => [
-            "parent_id" => ""
-        ]
+        "model" => "root",
+        "parent_id" => ""
     ]);
     $root->save();
-/*
-    $book = new \App\Models\Book([
-        "entity" =>  [
-            "parent_id" =>  "the-category-id",
-            "active" =>  false
-        ],
-        "contents" => [
-            "title" => "Libro 1"
-        ]
-    ]);
-    $book->save();
-
-    $book2 = \App\Models\Book::create([
-      "entity" =>  [
-          "parent_id" =>  "the-category-id-2",
-          "active" =>  false
-      ],
-      "contents" => [
-        "title" => "Libro 2 T1",
-        "summary" => "Libro 2 S1",
-        "es" => [
-            "title" => "Libro 2 T1 en español"
-        ]
-      ]
-    ]);
-    $book2->save();
-
-    $book2->contents = [
-        "title" => "Libro 2 T2",
-        "summary" => "Libro 2 S2",
-        "description" => "Libro 2 D2",
-        "fr" => [
-            "title" => "Libro 2 T2 en francés"
-        ]
-    ];
-    $book2->entity = [
-        "parent_id" => "nuevo"
-    ];
-    $book2->save();
-*/
-
-    // $books = \App\Models\Book::all();
-
-    // var_dump($books->toArray());
-
-
-
-    /*$book = new \App\Models\Book();
-    $book->save();
-
-    \App\Models\Book::create();
-
-    $book2 = new Entity();
-    $book2->model = "book";
-    $book2->save();*/
   }
 }
