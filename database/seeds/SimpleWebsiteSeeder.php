5268b61f71f704c3dd3ce68a99959d987e8cfbef
Ramses <ramses@cuatromedios.com>
2018-12-11 14:29:02 -0600

    Reduced the models complexity to use more Eloquent's features

--- database/seeds/SimpleWebsiteSeeder.php
+++ database/seeds/SimpleWebsiteSeeder.php
@@ -5,6 +5,7 @@ use App\Models\Home;
 use App\Models\User;
 use App\Models\Entity;
 use Cuatromedios\Kusikusi\Models\Relation;
+use Cuatromedios\Kusikusi\Models\EntityContent;
 
 class SimpleWebsiteSeeder extends Seeder
 {
@@ -15,218 +16,39 @@ class SimpleWebsiteSeeder extends Seeder
    */
   public function run()
   {
-    $admin = new User([
-        "name" => "Admin",
-        "email" => "admin",
-        "password" => "admin",
-        "profile" => User::PROFILE_ADMIN,
-      "contents" => [
-          "cv" => "El nombre completo"
-      ]
-    ]);
-    $admin->save();
-    Entity::create([
+
+    $homeEntity = new Entity([
         "model" => Home::modelId(),
         "id" => Home::modelId(),
         "name" => ucfirst(Home::modelId()),
         "parent_id" => 'root',
-        "contents" => [
-            "title" => "Home"
-        ]
     ]);
-    
-    // $home->relations()->attach('root', ['kind' => 'ancestor', 'position' => '1']);
+    $homeEntity->contents()->save(new EntityContent(["field" => "title", "value"=>"El home", "lang" => "en"]));
+    $homeEntity->contents()->save(new EntityContent(["field" => "summary", "value"=>"Bienvenidos"]));
+    $homeEntity->contents()->save(new EntityContent(["footer" => "Teléfono"]));
+    $homeEntity->addContents(["keywords" => "a,b,c"]);
+    $homeEntity->save();
 
-    $notHome = Entity::create([
-        "model" => Home::modelId(),
-        "id" => 'notHome',
-        "name" => 'Not Home',
-        "parent_id" => $home->id,
-        "contents" => [
-            "title" => "Not Home"
-        ]
+
+    $adminEntity = new Entity([
+        "model" => "user",
+        "parent_id" => $homeEntity->id
     ]);
+    $adminEntity->addContents(["cv" => "The user CV", "bio" => "My bio"]);
+    $adminEntity->addRelation(['id' => $homeEntity->id, 'kind' => 'home', 'tags' => '1,2,3', 'depth'=>3, 'position'=>4]);
 
-    // $notHome->relations()->attach('home', ['kind' => 'ancestor', 'position' => '1']);
-
-    $notHome->addRelation(['id' => 'root']);
-    // $notHome->addRelation(['id' => 'root', 'relation' => ['kind' => 'ancestor']]);
-
-    $admin = new User([
+    $adminUser = new User([
         "name" => "Admin",
         "email" => "admin",
         "password" => "admin",
         "profile" => User::PROFILE_ADMIN,
-        "entity" => [
-            "parent_id" => 'home'
-        ],
-        "contents" => [
-            "cv" => "El nombre completo"
-        ]
       ]);
-      $admin->save();
+    $adminEntity->user()->save($adminUser);
+    $adminEntity->save();
 
-      $notHome->addRelation(['id' => $admin->id]);
-    $home = Entity::find('home');
-    $home->contents = [
-        "summary" => "The home summary"
-    ];
-    $home->save();
+    $homes = Entity::ofModel('user')->select('id')->with('user')->with(['contents' => function($query) {$query->where('field', '=', 'summary')->orWhere('field', '=', 'cv');}])->get();
 
-    $user = User::find($admin->id);
-    var_dump($user->toArray());
-    $eUser = Entity::find($admin->id);
-    var_dump($eUser->toArray());
-
-
-    /*
-    $section = new \App\Models\Section([
-        'parent_id' => $home->id
-    ]);
-    $section->save();
-    \App\Models\Section::create([
-       'parent_id' => $home->id
-    ]);
-    */
-    /*$u1 = new App\Models\User([
-        'name' => 'user'
-    ]);
-    $u1->save();
-    print(" 1: ".$u1->id);
-    $u1->contents()->saveMany([
-       new EntityContent(['lang' => 'es', 'field' => 'title', 'value' => 'Home'])
-    ]);
-    */
-
-    /*$u2 = new App\Models\Entity([
-        'name' => 'user'
-    ]);
-    $u2->save();
-    print(" 2: ".$u2->id);
-    $u2->user()->create(["name" => "hugo"]);
-    */
-
-
-    /*
-      $home = App\Models\Home([
-          'id' => 'home',
-          'model' => 'home',
-          'created_by' => 'seeder',
-          'updated_by' => 'seeder',
-          'parent_id' => 'root',
-          'contents' => [
-              'title' => 'Website title',
-              'description' => 'The website description',
-              'url' => '/',
-              ['lang' => 'es', 'field' => 'summary', 'value' => 'El otro titulo'],
-              ['lang' => 'en', 'field' => 'summary', 'value' => 'The other title']
-          ]
-      ]);
-    */
-    /*
-    $eSection = EntityBase::create([
-        'model' => 'section',
-        'parent' => $eHome['id'],
-        'created_by' => 'seeder',
-        'updated_by' => 'seeder',
-        'contents' => [
-            'title' => 'Section title',
-            'description' => 'Section description',
-            'url' => '/section',
-        ]
-    ]);
-
-    $ePage = EntityBase::create([
-        'model' => 'page',
-        'parent' => $eSection['id'],
-        'created_by' => 'seeder',
-        'updated_by' => 'seeder',
-        'contents' => [
-            'title' => 'Page title',
-            'description' => 'Section description',
-            'url' => '/section/page',
-        ]
-    ]);
-
-    $eMedia = EntityBase::create([
-        'id' => 'media',
-        'model' => 'container',
-        'parent' => 'root',
-        'created_by' => 'seeder',
-        'updated_by' => 'seeder',
-        'name' => 'Media Container'
-    ]);
-
-    for ($i = 0; $i < 5; $i++) {
-        $eMedium = EntityBase::create([
-            'model' => 'medium',
-            'parent' => $eMedia['id'],
-            'position' => $i,
-            'created_by' => 'seeder',
-            'updated_by' => 'seeder',
-            'contents' => [
-                'title' => 'Medium title '.$i
-            ],
-            'data' => [
-                'format' => 'jpg',
-                'size' => rand(0, 10000)
-            ]
-        ]);
-        $ePage->relations()->attach($eMedium['id'], ['kind' => 'medium', 'position' => $i]);
-    }
-
-          $eUsers = EntityBase::create([
-              'id' => 'users',
-              'model' => 'container',
-              'parent' => 'root',
-              'created_by' => 'seeder',
-              'updated_by' => 'seeder',
-              'name' => 'Users Container'
-          ]);
-
-          $generator = new RequirementPasswordGenerator();
-          $generator
-              ->setLength(12)
-              ->setOptionValue(RequirementPasswordGenerator::OPTION_UPPER_CASE, true)
-              ->setOptionValue(RequirementPasswordGenerator::OPTION_LOWER_CASE, true)
-              ->setOptionValue(RequirementPasswordGenerator::OPTION_NUMBERS, true)
-              ->setOptionValue(RequirementPasswordGenerator::OPTION_SYMBOLS, true)
-              ->setMinimumCount(RequirementPasswordGenerator::OPTION_LOWER_CASE, 2)
-              ->setMinimumCount(RequirementPasswordGenerator::OPTION_NUMBERS, 2)
-              ->setMinimumCount(RequirementPasswordGenerator::OPTION_SYMBOLS, 1)
-              ->setMaximumCount(RequirementPasswordGenerator::OPTION_UPPER_CASE, 5)
-              ->setMaximumCount(RequirementPasswordGenerator::OPTION_LOWER_CASE, 5)
-              ->setMaximumCount(RequirementPasswordGenerator::OPTION_NUMBERS, 5)
-              ->setMaximumCount(RequirementPasswordGenerator::OPTION_SYMBOLS, 2)
-          ;
-          $adminPassword = $generator->generatePassword();
-          $adminUsername = 'admin';
-          print ("***************** \n");
-          print ("* Admin username: ".$adminUsername)."\n";
-          print ("* Admin password: ".$adminPassword)."\n";
-          print ("***************** \n");
-          $eAdminUser = EntityBase::create([
-              'model' => 'user',
-              'name' => 'Admin',
-              'parent' => $eUsers['id'],
-              'created_by' => 'seeder',
-              'updated_by' => 'seeder',
-              'data' => [
-                  'name' => 'Admin',
-                  'email' => $adminUsername,
-                  'password' => $adminPassword,
-                  'profile' => User::PROFILE_ADMIN
-              ]
-          ]);
-
-           Permission::create([
-              'user_id'   => $eAdminUser['id'],
-              'entity_id' => 'root',
-              'read'       => 'any',
-              'write'      => 'any'
-          ]);
-
-    $eAdminUser->relations()->attach($eHome['id'], ['kind' => 'home', 'position' => 0]);
-    */
+    $grouped = array_map("Cuatromedios\Kusikusi\Models\EntityContent::reduce", $homes->toArray());
+    var_dump($grouped);
   }
 }
