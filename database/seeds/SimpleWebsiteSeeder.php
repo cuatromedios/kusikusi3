07068bd9e8a34d701d71238f9f2949a34b00a905
Ramses <ramses@cuatromedios.com>
2018-12-12 08:59:33 -0600

    Scope to select the content fields to return using ->withContents('field1', 'field2')

--- database/seeds/SimpleWebsiteSeeder.php
+++ database/seeds/SimpleWebsiteSeeder.php
@@ -61,15 +61,13 @@ class SimpleWebsiteSeeder extends Seeder
         ->get();
 
     $grouped = EntityContent::compact($homes->toArray());
-    print(json_encode($grouped, JSON_PRETTY_PRINT));
-
-     print("*******\n");
+    // print(json_encode($grouped, JSON_PRETTY_PRINT));
 
     $user = User::with("entity.contents")->first();
 
-     print(json_encode(EntityContent::compact($user), JSON_PRETTY_PRINT));
+     // print(json_encode(EntityContent::compact($user), JSON_PRETTY_PRINT));
 
-     $all = Entity::with('contents')->get();
+     $all = Entity::withContents('bio')->with('user')->get();
     print(json_encode(EntityContent::compact($all), JSON_PRETTY_PRINT));
   }
 }
