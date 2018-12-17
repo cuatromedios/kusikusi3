f6499d88f3a7c85dda8ef538287cc8930dc4cfb0
Ramses <ramses@cuatromedios.com>
2018-12-16 20:27:50 -0600

    Simple website sample, with images, simple navigation, using blade templates.

--- resources/views/html.blade.php
+++ resources/views/html.blade.php
@@ -3,6 +3,11 @@
     <title>@yield('title')</title>
 </head>
 <body>
+    <nav>
+        @foreach ($ancestors as $ancestor)
+            &raquo; <a href="{{ $ancestor['contents']['url'] ?? '' }}">{{ $ancestor['contents']['title'] ??'' }}</a>
+        @endforeach
+    </nav>
     <main>
         @yield('main')
     </main>
