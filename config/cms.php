a3fee0c0c3b39b6ca5cc89914f0d4ed4e156dbe9
Ramses <ramses@cuatromedios.com>
2019-05-15 15:14:35 -0500

    Fields in the front end are now generated based on the configuration. The content fields, that are multilanguage, are repeated for every language. Special commit functions so they are used in components but affecting the Vuex store.

--- config/cms.php
+++ config/cms.php
@@ -10,308 +10,64 @@
 return [
     'langs' => ['en', 'es', 'fr', 'de'], // The first lang will be the default each time the entity is loaded
     'models' => [
-        'root' => [
-            'name' => 'Root',
-            'display' => [
-                [
-                    'component' => 'children',
-                    'props' => [
-                        'label' => 'Hijos',
-                        'allowed' => [],
-                        'order'  => 'e.position asc'
-                    ]
-                ],
-                [
-                    'component' => 'children',
-                    'props' => [
-                        'label' => 'Solo contenedores',
-                        'allowed' => ['container'],
-                        'filter' => ['model:container'],
-                        'order'  => 'e.position asc'
-                    ]
-                ],
-            ]
-        ],
-        'container' => [
-            'name' => 'Container',
-            'display' => [
-                [
-                    'component' => 'children',
-                    'props' => [
-                        'label' => 'Contenidos',
-                    ]
-                ]
-            ]
-        ],
-        'user' => [
-            'name' => 'User',
-            'display' => [
-                [
-                    'component' => 'entityCard',
-                ]
-            ],
-            'editor' => [
-                [
-                    'component' => 'formHeader',
-                    'props' => [
-                        'field' => 'name',
-                        'level' => 3
-                    ]
-                ],
-                [
-                    'component' => 'formHeader',
-                    'props' => [ 'text' => 'User Basic Data', 'level' => 5 ]
-                ],
-                [
-                    'component' => 'userBasicData',
-                ],
-                [
-                    'component' => 'formHeader',
-                    'props' => [ 'text' => '---------------------------------------------------------------------------------', 'level' => 3]
-                ],
-                [
-                    'component' => 'publication'
-                ],
-            ]
-        ],
-        'medium' => [
-            'name' => 'Medium',
-            'display' => [
-                [
-                    'component' => 'entityCard',
-                ]
-            ],
-            'editor' => [
-                [
-                    'component' => 'formHeader',
-                    'props' => [
-                        'field' => 'name',
-                        'level' => 3
-                    ]
-                ],
-                [
-                    'component' => 'displayMedia',
-                ],
-                [
-                    'component' => 'publication'
-                ],
-            ]
-        ],
         'home' => [
             'name' => "Home",
             'icon' => "home",
             'display' => [
                 [
-                    'component' => 'entityCard',
-                    'props' => [
-                        'titleSize' => 3
-                    ]
+                    'component' => 'EntityCard'
                 ],
                 [
-                    'component' => 'children',
+                    'component' => 'Children',
                     'props' => [
-                        'label' => 'In this section',
+                        'label' => 'In home:',
                         'allowed' => ['section', 'page'],
-                        'order'  => 'e.position asc',
-                        'tags' => ['example', 'ejemplo', 'exemple', 'beispiel']
+                        'order' => 'e.position asc',
+                        'tags' => ['menu']
                     ]
                 ],
             ],
             'editor' => [
                 [
-                    'component' => 'formHeader',
-                    'props' => [
-                        'field' => 'contents.title',
-                        'level' => 3
+                    'groupName' => 'content.editor.contents',
+                    'fields' => [
+                        [
+                            'label' => 'content.editor.name',
+                            'component' => 'TextField',
+                            'field' => 'name',
+                            'props' => [ ]
+                        ],
+                        [
+                            'label' => 'content.editor.title',
+                            'component' => 'TextField',
+                            'field' => 'contents.title',
+                            'props' => [ 'size' => 2 ]
+                        ],
+                        [
+                            'label' => 'content.editor.summary',
+                            'component' => 'TextField',
+                            'field' => 'contents.summary',
+                            'props' => [ 'lines' => 3 ]
+                        ]/*,
+                        [
+                            'label' => 'content.editor.content',
+                            'component' => 'MultilangWysiwyg',
+                            'field' => 'contents.content',
+                            'props' => [ 'lines' => 15 ]
+                        ]*/
                     ]
-                ],
+                ]/*,
                 [
-                    'component' => 'multiLang'
-                ],
-                [
-                    'component' => 'titleSummaryContent',
-                ],
-                [
-                    'component' => 'textInput',
-                    'props' => [
-                        'label' => 'multilinea',
-                        'field' => 'contents.summary',
-                        'params' => [
-                            'type' => 'textarea',
-                            'rows' => 3
+                    'groupName' => 'content.editor.url',
+                    'fields' => [
+                        [
+                            'label' => 'content.editor.url',
+                            'component' => 'url',
+                            'field' => 'contents.url',
+                            'props' => [ ]
                         ]
                     ]
-                ],
-                [
-                    'component' => 'urlAccess'
-                ],
-                [
-                    'component' => 'formHeader',
-                    'props' => [ 'text' => 'Publicacion', 'level' => 4 ]
-                ],
-                [
-                    'component' => 'toggleButton',
-                    'props' => [
-                        'label' => 'Boton boleano',
-                        'field' => 'position',
-                        'trueValue' => [
-                            'label' => 'veinte',
-                            'value' => 20
-                        ],
-                        'falseValue' => [
-                            'label' => 'cinco',
-                            'value' => 5
-                        ],
-                    ]
-                ],
-                [
-                    'component' => 'publication'
-                ],
-                [
-                    'component' => 'media',
-                    'props' => [
-                        'tags' => ['tag1', 'tag2', 'cover', 'icon', 'ejemplo'],
-                        'filter' => ['.jpg', '.png']
-                    ]
-                ],
-                [
-                    'component' => 'relation',
-                    'props' => [
-                        'label' => 'Relaciones de medium',
-                        'kind' => ['medium','ancestor'],
-                        'childrenOf' => 'media',
-                        'tags' => ['example', 'ejemplo', 'exemple', 'beispiel']
-                    ]
-                ]
-            ]
-        ],
-        'section' => [
-            'name' => "Section",
-            'display' => [
-                [
-                    'component' => 'entityCard',
-                    'props' => [
-                        'titleSize' => 3
-                    ]
-                ],
-                [
-                    'component' => 'children',
-                    'props' => [
-                        'label' => 'Hijos',
-                        'allowed' => ['page'],
-                        'order'  => 'e.position asc'
-                    ]
-                ],
-            ],
-            'editor' => [
-                [
-                    'component' => 'formHeader',
-                    'props' => [
-                        'field' => 'contents.title',
-                        'level' => 3
-                    ]
-                ],
-                [
-                    'component' => 'multiLang'
-                ],
-                [
-                    'component' => 'titleSummaryContent',
-                    'props' => [
-                        'multilanguage' => true
-                    ]
-                ],
-                [
-                    'component' => 'urlAccess'
-                ],
-                [
-                    'component' => 'selectInput',
-                    'props' => [
-                        'label' => 'Prueba',
-                        'field' => 'contents.summary',
-                        'placeholder' => 'Selecciona algo',
-                        'options' => [
-                            [
-                                'label' => 'Opcion 1',
-                                'value' => 'Elegiste la primer opcion'
-                            ],
-                            [
-                                'label' => 'Opcion 2',
-                                'value' => 'Elegiste la segunda de las opciones'
-                            ]
-                        ]
-                    ]
-                ],
-                [
-                    'component' => 'formHeader',
-                    'props' => [ 'text' => 'TEST', 'level' => 5 ]
-                ],
-                [
-                    'component' => 'publication'
-                ],
-                [
-                    'component' => 'qrCode',
-                    'props' => [ 'prefix' => 'http://kusikusi.com/']
-                ],
-                [
-                    'component' => 'media',
-                    'props' => [
-                        'tags' => ['tag1', 'tag2', 'tag3', 'tag4'],
-                        'filter' => ['.gif']
-                    ]
-                ]
-            ]
-        ],
-        'page' => [
-            'name' => "Page",
-            'display' => [
-                [
-                    'component' => 'entityCard',
-                    'props' => [
-                        'titleSize' => 2
-                    ]
-                ]
-            ],
-            'editor' => [
-                [
-                    'component' => 'formHeader',
-                    'props' => [
-                        'field' => 'contents.title',
-                        'level' => 3
-                    ]
-                ],
-                [
-                    'component' => 'multiLang'
-                ],
-                [
-                    'component' => 'titleSummaryContent'
-                ],
-                [
-                    'component' => 'urlAccess'
-                ],
-                [
-                    'component' => 'media',
-                    'props' => [
-                        'tags' => ['imagen', 'otro']
-                    ]
-                ],
-                [
-                    'component' => 'publication'
-                ],
-                [
-                    'component' => 'qrCode'
-                ],
-                [
-                    'component' => 'formHeader',
-                    'props' => ['text' => 'H2', 'level' => 2]
-                ],
-                [
-                    'component' => 'formHeader',
-                    'props' => ['text' => 'H3', 'level' => 3]
-                ],
-                [
-                    'component' => 'formHeader',
-                    'props' => ['text' => 'H4', 'level' => 4]
-                ],
+                ]*/
             ]
         ]
     ]
