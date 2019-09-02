45c598c189cdbed1bd0df7bae64dd2ee394f068a
Ramses <ramses@cuatromedios.com>
2019-09-02 08:55:04 -0500

    Adding mix-manifest to the repository and removing extra css classes for the startitng instalation

--- resources/views/html/home.blade.php
+++ resources/views/html/home.blade.php
@@ -2,7 +2,6 @@
 @section('title', $entity['contents']['title'] ?? '')
 @section('main')
     <h1>{{ $entity['contents']['title'] ?? '' }}</h1>
-    <img src="/img/logo.svg" class="logo">
     <p>{{ $entity['contents']['summary'] ?? '' }}</p>
     <ul>
         @forelse ($children as $child)
